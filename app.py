7ed306ebba3df02b4d5cb2953faa6d19a2bc832c
Jason-Lee-07 <8jcgs84h9v@privaterelay.appleid.com>
2026-08-12 02:32:39 +0700

    Update app.py

--- app.py
+++ app.py
@@ -64,9 +64,9 @@ st.caption("Hệ thống RAG tự động cập nhật tin tức & trích dẫn 
 
 @st.cache_resource
 def init_rag_chain():
-    embeddings = GoogleGenerativeAIEmbeddings(
+embeddings = GoogleGenerativeAIEmbeddings(
     model="models/text-embedding-004",
-    google_api_key=st.secrets["AQ.Ab8RN6Kf32iFopL5JZctllBGjGggmf3pVRfxjvaZFnF8jrwa9Q"]
+    google_api_key=st.secrets["GOOGLE_API_KEY"]
 )
     vector_db = Chroma(
         persist_directory="./my_vector_db",
